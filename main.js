fc4686c0efc14fefb17bb835e257ea6a4bb0591e
Sam George Mathew <37585951+SGM-dev@users.noreply.github.com>
2023-09-06 22:00:49 +0530

    get movie details and render

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const searchBtn = document.getElementById("search-btn");
 const form = document.getElementById("search-form");
 const searchInput = document.getElementById("search-input");
+const mainContainer = document.getElementById("main-container");
 
 const apiKey = "98889bce";
 let movieIDs;
@@ -17,5 +18,37 @@ async function getMovieIds(input) {
 
   const data = await response.json();
   movieIDs = data.Search.map((movie) => movie.imdbID);
-  console.log(movieIDs)
+  getMovieDetails(movieIDs);
+}
+
+function getMovieDetails(arr) {
+  mainContainer.innerHTML = ``;
+
+  arr.forEach(async (imdbID) => {
+    const response = await fetch(
+      `https://www.omdbapi.com/?apikey=${apiKey}&i=${imdbID}`
+    );
+    const data = await response.json();
+    mainContainer.innerHTML += `
+    <article class="search-result">
+    <img
+      alt="${data["Title"]} poster"
+      class="movie-poster"
+      src="${data["Poster"]}"
+    />
+    <div class="movie-info">
+      <div class="movie-header">
+        <h3 class="movie-title">${data["Title"]}</h3>
+      </div>
+      <div class="movie-subinfo">
+        <p class="runtime">${data["Runtime"]}</p>
+        <p class="genre">${data["Genre"]}</p>
+        <button class="add-to-watchlist">Watchlist</button>
+      </div>
+      <p class="plot">
+      ${data["Plot"]}
+      </p>
+    </div>
+  </article>`;
+  });
 }
